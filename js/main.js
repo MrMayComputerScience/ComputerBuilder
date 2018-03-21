6652413f656f9527c2da0b09d41afb4f0a2d154b
tangdavid <tang.david.2000@gmail.com>
2018-03-21 14:51:56 -0500

    nothing

--- js/main.js
+++ js/main.js
@@ -11,13 +11,49 @@ function makeDraggables(){
 			autoScroll: true,
 			onmove: dragMoveListener
 		});
+	interact(".dropzone")
+		interact('.dropzone').dropzone({
+		  // only accept elements matching this CSS selector
+		  accept: '#yes-drop',
+		  // Require a 75% element overlap for a drop to be possible
+		  overlap: 0.75,
+
+		  // listen for drop related events:
+
+		  ondropactivate: function (event) {
+		    // add active dropzone feedback
+		    event.target.classList.add('drop-active');
+		  },
+		  ondragenter: function (event) {
+		    var draggableElement = event.relatedTarget,
+		        dropzoneElement = event.target;
+
+		    // feedback the possibility of a drop
+		    dropzoneElement.classList.add('drop-target');
+		    draggableElement.classList.add('can-drop');
+		    draggableElement.textContent = 'Dragged in';
+		  },
+		  ondragleave: function (event) {
+		    // remove the drop feedback style
+		    event.target.classList.remove('drop-target');
+		    event.relatedTarget.classList.remove('can-drop');
+		    event.relatedTarget.textContent = 'Dragged out';
+		  },
+		  ondrop: function (event) {
+		    event.relatedTarget.textContent = 'Dropped';
+		  },
+		  ondropdeactivate: function (event) {
+		    // remove active dropzone feedback
+		    event.target.classList.remove('drop-active');
+		    event.target.classList.remove('drop-target');
+		  }
+		});
 }
 function dragMoveListener(evt){
 	console.log("help");
 	var target = evt.target, x, y;
 	x = (parseFloat(target.getAttribute("data-x")) || 0) + evt.dx;
 	y = (parseFloat(target.getAttribute("data-y")) || 0) + evt.dy;
-	target.style.webkitTransform = 
 	target.style.transform = "translate("+x+"px, "+y+"px)";
 	target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
