add2dd12ece96499bd3af2867b3968976e410cf3
Mason McCully <mmccully2000@gmail.com>
2018-03-29 14:04:37 -0500

    particles

--- js/main.js
+++ js/main.js
@@ -2,14 +2,20 @@
 
 //Function to retrieve the CSS property of an object as it was defined in its CSS file
 //@returns - a string representation of the property value
-function getDefaultStyle(element, prop) {
-    var parent = element.parentNode,
-        computedStyle = getComputedStyle(element),
-        value;
-    parent.style.display = 'none';
-    value = computedStyle.getPropertyValue(prop);
-    parent.style.removeProperty('display');
-    return value;
+function getStyleProperty(element, prop) {
+    if(element.style.getPropertyValue(prop)){
+        return element.style.getPropertyValue(prop);
+    }
+    else{
+        var parent = element.parentNode,
+            computedStyle = getComputedStyle(element),
+            value;
+        parent.style.display = 'none';
+        value = computedStyle.getPropertyValue(prop);
+        parent.style.removeProperty('display');
+        return value;
+    }
+    
 }
 
 //Abstraction of dropzone code. Sets all elements that match the CSS selector string "selector"
@@ -22,8 +28,12 @@ function setDropzone(selector, accepts){
             //  -Make so dz must be fully covered, pct of area??
 			overlap: 0.4,
 			ondrop: function(evt){
+                var target = evt.target;
 				evt.relatedTarget.classList.remove("draggable");
-                
+                let w = parseFloat(getStyleProperty(target, "width")),
+                    h = parseFloat(getStyleProperty(target, "height"));
+                console.log("w: "+w + ", h: "+h);
+                particleExplosion(evt.target, 0.5*w, 0.5*h, 10);
 			},
 	});
 }
@@ -31,7 +41,6 @@ function setDropzone(selector, accepts){
 //Function callen when the body in index.html loads. 
 //It initializes the drag-n-drop features and adds window listeners
 function onBodyLoad(){
-    
 	interact(".draggable")
 		.draggable({
 			inertia: false,
@@ -54,13 +63,14 @@ function dragMoveListener(evt){
     
     //If the inline style has already been modified, base the change on that
     //else, use the default value
+    //UPDATE: this if/else is now deprecated because I abstracted this code to the getStyleProperty function
     if(target.style.top){
         x = parseFloat(target.style.left) + evt.dx;
         y = parseFloat(target.style.top) + evt.dy;    
     }
     else{
-        x = parseFloat(getDefaultStyle(target, "left")) + evt.dx;
-        y = parseFloat(getDefaultStyle(target, "top")) + evt.dy;
+        x = parseFloat(getStyleProperty(target, "left")) + evt.dx;
+        y = parseFloat(getStyleProperty(target, "top")) + evt.dy;
     }
     //IMPORTANT ERROR CHECKING
     if(isNaN(x) || isNaN(y))
--- js/particles.js
+++ js/particles.js
@@ -1,20 +1,55 @@
 const __particleSize__ = {
-	w: "1%",
-	h: "1%"
+	w: "40px",
+	h: "40px"
 };
 const __lifeTime__ = 2;
 function __createParticle__(fileName, parent, xpos, ypos){
 	var img = document.createElement("img");
 	img.src = "img/"+fileName;
+    img.style.position = "absolute";   
 	img.style.width = __particleSize__.w;
 	img.style.height = __particleSize__.h;
-	img.setAttribute("x-pos", xpos);
-	img.setAttribute("y-pos", ypos);
+    img.style.top = ypos+"px";
+    img.style.left = xpos+"px";
+    console.log("Creation: "+ xpos + ", " + ypos);
 	parent.appendChild(img);
+    return img;
 }
 
-function particleExplosion(parent, numParticles){
+function particleExplosion(parent, cx, cy, numParticles){
+    var angle = 360/numParticles;
+    var particles = [];
+    
 	for(let i = 0; i < numParticles; i++){
-		__createParticle__("particle.png", parent);
+		let img = __createParticle__("particle.png", parent, cx, cy);
+        img.setAttribute("angle", angle*i);
+        particles[i] = img;
 	}
+    
+    var pos = 0;
+    var id = setInterval(() => {
+        if(pos >= 20){
+            clearInterval(id);
+            for(let i = 0; i < particles.length; i++){
+                parent.removeChild(particles[i]);
+            }
+        }
+        else{
+            pos++;
+            console.log(pos);
+            for(let i = 0; i < particles.length; i++){
+                let currAngle = particles[i].getAttribute("angle");
+                currAngle = parseFloat(currAngle);
+                let x = parseFloat(particles[i].style.left);
+                let y = parseFloat(particles[i].style.top);
+                console.log(x + ", "+ y);
+                x += pos*Math.cos(currAngle);
+                y += pos*Math.sin(currAngle);
+                particles[i].style.left = x+"px";
+                particles[i].style.top = y+"px";
+                
+            }
+        }
+    },20)
+    
 }
